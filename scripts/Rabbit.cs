69b196328b6e6106e9c56c64ae8dd3b57536036a
Георгий Светличный <sv-giorgio@MacBook-Pro-Georgij.local>
2025-12-10 03:43:31 +0300

    Add hat with rabbits spawning system. Rabbits spawn only after player moves away from start position.

---
+++ scripts/Rabbit.cs
@@ -0,0 +1,91 @@
+using Godot;
+using System;
+using System.Reflection.Metadata;
+using System.Threading.Tasks;
+
+public partial class Rabbit : CharacterBody2D
+{
+	public const float Speed = 100.0f;
+	public const float ExploisonRadius = 100.0f;
+	public const int ExploisonDamage = 15;
+
+	private bool chaseState = false;
+
+	private bool isExploiding = false;
+	
+	[Export] public AnimatedSprite2D RabbitAnim;
+	public override void _Ready()
+	{	
+
+		Area2D _detect = GetNode<Area2D>($"./Detector");
+		_detect.Connect(Area2D.SignalName.BodyEntered, Callable.From<Node>(OnBodyEntered));
+		Area2D _detect_boom = GetNode<Area2D>($"./Detector_BOOM");
+		_detect_boom.Connect(Area2D.SignalName.BodyEntered, Callable.From<Node>(OnBoomEntered));
+
+		RabbitAnim.Play("move");
+	}
+	public override void _PhysicsProcess(double delta)
+	{
+		if (isExploiding) return;
+
+		CharacterBody2D _player = GetNode<CharacterBody2D>($"../Player");
+		
+		Vector2 velocity = Velocity;
+
+		Vector2 direction;
+		direction.X = Math.Sign(_player.Position.X - this.Position.X);
+		direction.Y = Math.Sign(_player.Position.Y - this.Position.Y);
+		if ((direction != Vector2.Zero) && (chaseState))
+		{
+			velocity.X = direction.X * Speed;
+			velocity.Y = direction.Y * Speed;
+		}
+
+		if ((direction.X< 0) || (Math.Abs(_player.Position.X - this.Position.X) < 10))
+		{
+			RabbitAnim.FlipH = true;
+		}
+		else
+		{
+			RabbitAnim.FlipH = false;
+		}
+
+		Velocity = velocity;
+		MoveAndSlide();
+	}
+
+	private void OnBodyEntered(Node body)
+	{
+		if (!(body is CharacterBody2D))
+		{
+			return;
+		}
+
+		if (body.Name == "Player")
+		{
+			chaseState = true;
+		}
+	}
+	private void OnBoomEntered(Node body)
+	{
+		if (!(body is CharacterBody2D))
+		{
+			return;
+		}
+
+		if (body.Name == "Player")
+		{	
+
+			isExploiding = true;
+			Death();
+		}
+	}
+
+	private async void Death()
+	{	
+		RabbitAnim.Play("death");
+		await ToSignal(RabbitAnim, "animation_finished");
+		
+		QueueFree();
+	}
+}
